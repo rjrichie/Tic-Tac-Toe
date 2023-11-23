132a43c71388547a2b62038d9664fe020a1e5f94
Richie Jonathan <richiejonathande@gmail.com>
2023-11-23 14:05:49 +0100

    Added Intro Page

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Test")
+
+def intro():
+    welcome = '''
+            =====================================================================================================================================================
+            ==    TTTTTTTTTT  IIIIIIII  CCCCCCCCC               TTTTTTTTTT      AAA         CCCCCCCCC                 TTTTTTTTTT   OOOOOOOOOO   EEEEEEEEEE     ==
+            ==        TT         II     CCCC                        TT         AAAAA        CCCC                          TT       OO      OO   EE             ==
+            ==        TT         II     CCC          ======         TT        AA   AA       CCC           ======          TT       OO      OO   EE             ==
+            ==        TT         II     CCC          ======         TT       AA     AA      CC            ======          TT       OO      OO   EEEEEEEEE      ==
+            ==        TT         II     CCC          ======         TT      AAAAAAAAAAA     CCC           ======          TT       OO      OO   EE             ==
+            ==        TT         II     CCCC                        TT     AA         AA    CCCC                          TT       OO      OO   EE             ==
+            ==        TT      IIIIIIII  CCCCCCCCC                   TT    AA           AA   CCCCCCCCC                     TT       OOOOOOOOOO   EEEEEEEEEE     ==
+            =====================================================================================================================================================
+                '''
+    print(welcome)
+
+
+
+intro()
